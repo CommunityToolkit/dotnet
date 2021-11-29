32c23450f9d2d8450d28dc7e1ef5e00b22a563f6
Sergio Pedri <sergio0694@live.com>
2021-11-29 13:31:49 +0100

    Fix some bugs in StrongReferenceMessenger.Unregister[All] APIs

--- tests/CommunityToolkit.Mvvm.UnitTests/Test_Messenger.cs
+++ tests/CommunityToolkit.Mvvm.UnitTests/Test_Messenger.cs
@@ -6,7 +6,11 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Messaging;
+using CommunityToolkit.Mvvm.Messaging.Internals;
+using Microsoft.Collections.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CommunityToolkit.Mvvm.UnitTests;
@@ -449,7 +453,7 @@ public partial class Test_Messenger
 
 #if NETCOREAPP // Auto-trimming is disabled on .NET Framework
     [TestMethod]
-    public void Test_Messenger_AutoCleanup()
+    public void Test_WeakReferenceMessenger_AutoCleanup()
     {
         IMessenger messenger = new WeakReferenceMessenger();
 
@@ -495,6 +499,144 @@ public partial class Test_Messenger
         GC.KeepAlive(messenger);
     }
 #endif
+
+    [TestMethod]
+    public void Test_StrongReferenceMessenger_AutoTrimming_UnregisterAll()
+    {
+        StrongReferenceMessenger messenger = new();
+        IDictionarySlim recipientsMap = (IDictionarySlim)typeof(StrongReferenceMessenger).GetField("recipientsMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+        IDictionarySlim<Type2, object> typesMap = (IDictionarySlim<Type2, object>)typeof(StrongReferenceMessenger).GetField("typesMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+
+        RecipientWithSomeMessages recipientA = new();
+        RecipientWithSomeMessages recipientB = new();
+
+        messenger.Register<MessageA>(recipientA);
+
+        Assert.IsTrue(messenger.IsRegistered<MessageA>(recipientA));
+
+        // There's one registered handler
+        Assert.AreEqual(1, recipientsMap.Count);
+        Assert.AreEqual(1, typesMap.Count);
+
+        messenger.Register<MessageB>(recipientA);
+
+        Assert.IsTrue(messenger.IsRegistered<MessageB>(recipientA));
+
+        // There's two message types, for the same recipient
+        Assert.AreEqual(1, recipientsMap.Count);
+        Assert.AreEqual(2, typesMap.Count);
+
+        messenger.Register<MessageA>(recipientB);
+
+        Assert.IsTrue(messenger.IsRegistered<MessageA>(recipientB));
+
+        // Now there's two recipients too
+        Assert.AreEqual(2, recipientsMap.Count);
+        Assert.AreEqual(2, typesMap.Count);
+
+        messenger.UnregisterAll(recipientA);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA>(recipientA));
+        Assert.IsFalse(messenger.IsRegistered<MessageB>(recipientA));
+
+        // Only the second recipient is left, which has a single message type
+        Assert.AreEqual(1, recipientsMap.Count);
+        Assert.AreEqual(1, typesMap.Count);
+
+        messenger.UnregisterAll(recipientB);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageB>(recipientA));
+
+        // Now both lists should be empty
+        Assert.AreEqual(0, recipientsMap.Count);
+        Assert.AreEqual(0, typesMap.Count);
+    }
+
+    [TestMethod]
+    public void Test_StrongReferenceMessenger_AutoTrimming_UnregisterAllWithToken()
+    {
+        StrongReferenceMessenger messenger = new();
+        IDictionarySlim recipientsMap = (IDictionarySlim)typeof(StrongReferenceMessenger).GetField("recipientsMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+        IDictionarySlim<Type2, object> typesMap = (IDictionarySlim<Type2, object>)typeof(StrongReferenceMessenger).GetField("typesMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+
+        RecipientWithSomeMessages recipientA = new();
+        RecipientWithSomeMessages recipientB = new();
+
+        messenger.Register<MessageA, int>(recipientA, 42);
+        messenger.Register<MessageB, int>(recipientA, 42);
+        messenger.Register<MessageA, int>(recipientB, 42);
+
+        Assert.IsTrue(messenger.IsRegistered<MessageA, int>(recipientA, 42));
+        Assert.IsTrue(messenger.IsRegistered<MessageB, int>(recipientA, 42));
+        Assert.IsTrue(messenger.IsRegistered<MessageA, int>(recipientB, 42));
+
+        // There are two recipients, for two message types
+        Assert.AreEqual(2, recipientsMap.Count);
+        Assert.AreEqual(2, typesMap.Count);
+
+        messenger.UnregisterAll(recipientA, 42);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientA, 42));
+        Assert.IsFalse(messenger.IsRegistered<MessageB, int>(recipientA, 42));
+
+        // Only the second recipient is left again
+        Assert.AreEqual(1, recipientsMap.Count);
+        Assert.AreEqual(1, typesMap.Count);
+
+        messenger.UnregisterAll(recipientB, 42);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientB, 42));
+
+        // Now both lists should be empty
+        Assert.AreEqual(0, recipientsMap.Count);
+        Assert.AreEqual(0, typesMap.Count);
+    }
+
+    [TestMethod]
+    public void Test_StrongReferenceMessenger_AutoTrimming_UnregisterAllWithMessageTypeAndToken()
+    {
+        StrongReferenceMessenger messenger = new();
+        IDictionarySlim recipientsMap = (IDictionarySlim)typeof(StrongReferenceMessenger).GetField("recipientsMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+        IDictionarySlim<Type2, object> typesMap = (IDictionarySlim<Type2, object>)typeof(StrongReferenceMessenger).GetField("typesMap", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(messenger)!;
+
+        RecipientWithSomeMessages recipientA = new();
+        RecipientWithSomeMessages recipientB = new();
+
+        messenger.Register<MessageA, int>(recipientA, 42);
+        messenger.Register<MessageB, int>(recipientA, 42);
+        messenger.Register<MessageA, int>(recipientB, 42);
+
+        messenger.Unregister<MessageA, int>(recipientA, 42);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientA, 42));
+        Assert.IsTrue(messenger.IsRegistered<MessageB, int>(recipientA, 42));
+        Assert.IsTrue(messenger.IsRegistered<MessageA, int>(recipientB, 42));
+
+        // First recipient is still subscribed to MessageB.
+        // The second one has a single subscription to MessageA.
+        Assert.AreEqual(2, recipientsMap.Count);
+        Assert.AreEqual(2, typesMap.Count);
+
+        messenger.Unregister<MessageB, int>(recipientA, 42);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientA, 42));
+        Assert.IsFalse(messenger.IsRegistered<MessageB, int>(recipientA, 42));
+        Assert.IsTrue(messenger.IsRegistered<MessageA, int>(recipientB, 42));
+
+        // Only the second recipient is left
+        Assert.AreEqual(1, recipientsMap.Count);
+        Assert.AreEqual(1, typesMap.Count);
+
+        messenger.Unregister<MessageA, int>(recipientB, 42);
+
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientA, 42));
+        Assert.IsFalse(messenger.IsRegistered<MessageB, int>(recipientA, 42));
+        Assert.IsFalse(messenger.IsRegistered<MessageA, int>(recipientB, 42));
+
+        // Now both lists should be empty
+        Assert.AreEqual(0, recipientsMap.Count);
+        Assert.AreEqual(0, typesMap.Count);
+    }
 
     [TestMethod]
     [DataRow(typeof(StrongReferenceMessenger))]
@@ -579,12 +721,12 @@ public partial class Test_Messenger
     {
         public int As { get; private set; }
 
+        public int Bs { get; private set; }
+
         public void Receive(MessageA message)
         {
             As++;
         }
-
-        public int Bs { get; private set; }
 
         public void Receive(MessageB message)
         {
