32a17cf5b504bcdb57f154b898b3c9c87c5d61e9
Sergio Pedri <sergio0694@live.com>
2022-03-13 02:22:16 +0100

    Add AlsoBroadcastChangeAttribute type

---
+++ CommunityToolkit.Mvvm/ComponentModel/Attributes/AlsoBroadcastChangeAttribute.cs
@@ -0,0 +1,42 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics;
+
+namespace CommunityToolkit.Mvvm.ComponentModel;
+
+/// <summary>
+/// An attribute that can be used to support <see cref="ObservablePropertyAttribute"/> in generated properties, when applied to fields
+/// contained in a type that is either inheriting from <see cref="ObservableRecipient"/>, or annotated with <see cref="ObservableRecipientAttribute"/>.
+/// When this attribute is used, the generated property setter will also call <see cref="ObservableRecipient.Broadcast{T}(T, T, string?)"/>.
+/// This allows generated properties to opt-in into broadcasting behavior without having to fallback into a full explicit observable property.
+/// <para>
+/// This attribute can be used as follows:
+/// <code>
+/// partial class MyViewModel : ObservableRecipient
+/// {
+///     [ObservableProperty]
+///     [AlsoBroadcastChange]
+///     private string username;
+/// }
+/// </code>
+/// </para>
+/// And with this, code analogous to this will be generated:
+/// <code>
+/// partial class MyViewModel
+/// {
+///     public string Username
+///     {
+///         get => username;
+///         set => SetProperty(ref username, value, broadcast: true);
+///     }
+/// }
+/// </code>
+/// </summary>
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+[Conditional("MVVMTOOLKIT_KEEP_SOURCE_GENERATOR_ATTRIBUTES")]
+public sealed class AlsoBroadcastChangeAttribute : Attribute
+{
+}
