807dbcaf943dbd0adda78447fce7282313961992
Sergio Pedri <sergio0694@live.com>
2022-03-25 13:07:16 +0100

    Switch NullabilityAttributesGenerator to incremental

--- CommunityToolkit.Mvvm.SourceGenerators/Attributes/NullabilityAttributesGenerator.cs
+++ CommunityToolkit.Mvvm.SourceGenerators/Attributes/NullabilityAttributesGenerator.cs
@@ -5,53 +5,73 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using CommunityToolkit.Mvvm.SourceGenerators.Extensions;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 
 namespace CommunityToolkit.Mvvm.SourceGenerators;
 
 /// <summary>
 /// A source generator for necessary nullability attributes.
 /// </summary>
-[Generator]
-public sealed class NullabilityAttributesGenerator : ISourceGenerator
+[Generator(LanguageNames.CSharp)]
+public sealed class NullabilityAttributesGenerator : IIncrementalGenerator
 {
-    /// <inheritdoc/>
-    public void Initialize(GeneratorInitializationContext context)
-    {
-    }
-
-    /// <inheritdoc/>
-    public void Execute(GeneratorExecutionContext context)
-    {
-        AddSourceCodeIfTypeIsNotPresent(context, "System.Diagnostics.CodeAnalysis.NotNullAttribute");
-        AddSourceCodeIfTypeIsNotPresent(context, "System.Diagnostics.CodeAnalysis.NotNullIfNotNullAttribute");
-    }
+    /// <summary>
+    /// The <c>System.Diagnostics.CodeAnalysis.NotNullAttribute</c> metadata name.
+    /// </summary>
+    private const string NotNullAttributeMetadataName = "System.Diagnostics.CodeAnalysis.NotNullAttribute";
 
     /// <summary>
-    /// Adds the source for a given attribute type if it's not present already in the compilation.
+    /// The <c>System.Diagnostics.CodeAnalysis.NotNullIfNotNullAttribute</c> metadata name.
     /// </summary>
-    private void AddSourceCodeIfTypeIsNotPresent(GeneratorExecutionContext context, string typeFullName)
+    private const string NotNullIfNotNullAttributeMetadataName = "System.Diagnostics.CodeAnalysis.NotNullIfNotNullAttribute";
+
+    /// <inheritdoc/>
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         // Check that the target attributes are not available in the consuming project. To ensure that
         // this works fine both in .NET (Core) and .NET Standard implementations, we also need to check
         // that the target types are declared as public (we assume that in this case those types are from the BCL).
         // This avoids issues on .NET Standard with Roslyn also seeing internal types from referenced assemblies.
-        if (context.Compilation.HasAccessibleTypeWithMetadataName(typeFullName))
-        {
-            return;
-        }
 
+        // Check whether [NotNull] is available
+        IncrementalValueProvider<bool> isNotNullAttributeAvailable =
+            context.CompilationProvider
+            .Select(static (item, _) => item.HasAccessibleTypeWithMetadataName(NotNullAttributeMetadataName));
+
+        // Generate the [NotNull] type
+        context.RegisterConditionalSourceOutput(isNotNullAttributeAvailable, static context =>
+        {
+            string source = LoadAttributeSourceWithMetadataName(NotNullAttributeMetadataName);
+
+            context.AddSource(NotNullAttributeMetadataName, source);
+        });
+
+        // Check whether [NotNullIfNotNull] is available
+        IncrementalValueProvider<bool> isNotNullIfNotNullAttributeAvailable =
+            context.CompilationProvider
+            .Select(static (item, _) => item.HasAccessibleTypeWithMetadataName(NotNullIfNotNullAttributeMetadataName));
+
+        // Generate the [NotNullIfNotNull] type
+        context.RegisterConditionalSourceOutput(isNotNullIfNotNullAttributeAvailable, static context =>
+        {
+            string source = LoadAttributeSourceWithMetadataName(NotNullIfNotNullAttributeMetadataName);
+
+            context.AddSource(NotNullIfNotNullAttributeMetadataName, source);
+        });
+    }
+
+    /// <summary>
+    /// Gets the generated source for a specified attribute.
+    /// </summary>
+    private static string LoadAttributeSourceWithMetadataName(string typeFullName)
+    {
         string typeName = typeFullName.Split('.').Last();
         string filename = $"CommunityToolkit.Mvvm.SourceGenerators.EmbeddedResources.{typeName}.cs";
 
         Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(filename);
         StreamReader reader = new(stream);
 
-        string source = reader.ReadToEnd();
-
-        context.AddSource($"{typeFullName}.cs", SourceText.From(source, Encoding.UTF8));
+        return reader.ReadToEnd();
     }
 }
