faf1061c900db6dc4db7968009332995e83ebb5a
Sergio Pedri <sergio0694@live.com>
2022-09-15 01:20:42 +0200

    Add workaround for Roslyn 4.3 issues

--- CommunityToolkit.Mvvm.SourceGenerators/Polyfills/GeneratorAttributeSyntaxContext.cs
+++ CommunityToolkit.Mvvm.SourceGenerators/Polyfills/GeneratorAttributeSyntaxContext.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if !ROSLYN_4_3
+#if !ROSLYN_4_3_0_OR_GREATER
 
 using System.Collections.Immutable;
 
