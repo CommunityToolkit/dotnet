338172af682a5910ca33962211be1751d0dd2008
Sergio Pedri <sergio0694@live.com>
2022-09-16 04:04:21 +0200

    Suppress CA1001 warnings in MVVM Toolkit

--- CommunityToolkit.Mvvm/Input/AsyncRelayCommand.cs
+++ CommunityToolkit.Mvvm/Input/AsyncRelayCommand.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel.__Internals;
 using CommunityToolkit.Mvvm.Input.Internals;
 
-#pragma warning disable CS0618
+#pragma warning disable CS0618, CA1001
 
 namespace CommunityToolkit.Mvvm.Input;
 
