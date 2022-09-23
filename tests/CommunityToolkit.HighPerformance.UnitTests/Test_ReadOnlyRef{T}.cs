f99a08e0bdb31eb7f67cd64e93602c59cd6da130
Sergio Pedri <sergio0694@live.com>
2022-09-23 22:05:51 +0200

    Bump HighPerformance package to .NET 7

--- tests/CommunityToolkit.HighPerformance.UnitTests/Test_ReadOnlyRef{T}.cs
+++ tests/CommunityToolkit.HighPerformance.UnitTests/Test_ReadOnlyRef{T}.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET7_0_OR_GREATER
+
 using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,23 +13,6 @@ namespace CommunityToolkit.HighPerformance.UnitTests;
 public class Test_ReadOnlyRefOfT
 {
     [TestMethod]
-#if NETFRAMEWORK
-    public void Test_RefOfT_CreateRefOfT()
-    {
-        ReadOnlyFieldOwner model = new();
-        ReadOnlyRef<int> reference = new(model, model.Value);
-
-        Assert.IsTrue(Unsafe.AreSame(ref Unsafe.AsRef(model.Value), ref Unsafe.AsRef(reference.Value)));
-    }
-
-    /// <summary>
-    /// A dummy model that owns an <see cref="int"/> field.
-    /// </summary>
-    private sealed class ReadOnlyFieldOwner
-    {
-        public readonly int Value = 1;
-    }
-#else
     public void Test_RefOfT_CreateRefOfT()
     {
         int value = 1;
@@ -35,5 +20,6 @@ public class Test_ReadOnlyRefOfT
 
         Assert.IsTrue(Unsafe.AreSame(ref value, ref Unsafe.AsRef(reference.Value)));
     }
-#endif
 }
+
+#endif
