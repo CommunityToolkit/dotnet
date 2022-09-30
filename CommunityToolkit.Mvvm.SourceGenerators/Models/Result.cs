caae7b74177e93215c209c4317454c8693a29e08
Sergio Pedri <sergio0694@live.com>
2022-09-30 17:46:49 +0200

    Make Result<TValue> directly equatable

--- CommunityToolkit.Mvvm.SourceGenerators/Models/Result.cs
+++ CommunityToolkit.Mvvm.SourceGenerators/Models/Result.cs
@@ -5,7 +5,12 @@
 // This file is ported and adapted from ComputeSharp (Sergio0694/ComputeSharp),
 // more info in ThirdPartyNotices.txt in the root of the project.
 
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using CommunityToolkit.Mvvm.SourceGenerators.Extensions;
+using CommunityToolkit.Mvvm.SourceGenerators.Helpers;
 
 namespace CommunityToolkit.Mvvm.SourceGenerators.Models;
 
@@ -15,4 +20,33 @@ namespace CommunityToolkit.Mvvm.SourceGenerators.Models;
 /// <typeparam name="TValue">The type of the wrapped value.</typeparam>
 /// <param name="Value">The wrapped value for the current result.</param>
 /// <param name="Errors">The associated diagnostic errors, if any.</param>
-internal sealed record Result<TValue>(TValue Value, ImmutableArray<DiagnosticInfo> Errors);
+internal sealed record Result<TValue>(TValue Value, ImmutableArray<DiagnosticInfo> Errors)
+    where TValue : IEquatable<TValue>?
+{
+    /// <inheritdoc/>
+    public bool Equals(Result<TValue>? obj) => Comparer.Default.Equals(this, obj);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => Comparer.Default.GetHashCode(this);
+
+    /// <summary>
+    /// An <see cref="IEqualityComparer{T}"/> implementation for <see cref="Result{TValue}"/>.
+    /// </summary>
+    private sealed class Comparer : Comparer<Result<TValue>, Comparer>
+    {
+        /// <inheritdoc/>
+        protected override void AddToHashCode(ref HashCode hashCode, Result<TValue> obj)
+        {
+            hashCode.Add(obj.Value);
+            hashCode.AddRange(obj.Errors);
+        }
+
+        /// <inheritdoc/>
+        protected override bool AreEqual(Result<TValue> x, Result<TValue> y)
+        {
+            return
+                EqualityComparer<TValue>.Default.Equals(x.Value, y.Value) &&
+                x.Errors.SequenceEqual(y.Errors);
+        }
+    }
+}
