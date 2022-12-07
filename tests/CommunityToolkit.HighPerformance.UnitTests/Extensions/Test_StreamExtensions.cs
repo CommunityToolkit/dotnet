a712b4f739368dd87aac69447d23aaff27a5869b
Sergio Pedri <sergio0694@live.com>
2022-12-07 16:57:22 +0100

    Add unit tests for buffered StreamExtensions.Read<T>

--- tests/CommunityToolkit.HighPerformance.UnitTests/Extensions/Test_StreamExtensions.cs
+++ tests/CommunityToolkit.HighPerformance.UnitTests/Extensions/Test_StreamExtensions.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.IO;
-using CommunityToolkit.HighPerformance;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CommunityToolkit.HighPerformance.UnitTests.Extensions;
@@ -36,5 +35,84 @@ public class Test_StreamExtensions
         Assert.AreEqual(unchecked(uint.MaxValue * 324823489204ul), stream.Read<ulong>());
 
         _ = Assert.ThrowsException<InvalidOperationException>(() => stream.Read<long>());
+    }
+
+    // See https://github.com/CommunityToolkit/dotnet/issues/513
+    [TestMethod]
+    public void Test_StreamExtensions_ReadWrite_WithBufferedStream()
+    {
+        Stream stream = new BufferedStream();
+
+        stream.Write(true);
+        stream.Write(42);
+        stream.Write(3.14f);
+        stream.Write(unchecked(uint.MaxValue * 324823489204ul));
+
+        stream.Position = 0;
+
+        Assert.AreEqual(true, stream.Read<bool>());
+        Assert.AreEqual(42, stream.Read<int>());
+        Assert.AreEqual(3.14f, stream.Read<float>());
+        Assert.AreEqual(unchecked(uint.MaxValue * 324823489204ul), stream.Read<ulong>());
+
+        _ = Assert.ThrowsException<InvalidOperationException>(() => stream.Read<long>());
+    }
+
+    private sealed class BufferedStream : MemoryStream
+    {
+        private ReadOnlyMemory<byte> bufferedBytes;
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (this.bufferedBytes.IsEmpty)
+            {
+                this.bufferedBytes = ReadMoreBytes();
+            }
+
+            int bytesToCopy = Math.Min(this.bufferedBytes.Length, count);
+
+            this.bufferedBytes.Span.Slice(0, bytesToCopy).CopyTo(buffer.AsSpan(offset, count));
+            this.bufferedBytes = this.bufferedBytes.Slice(bytesToCopy);
+
+            return bytesToCopy;
+        }
+
+#if NET6_0_OR_GREATER
+        public override int Read(Span<byte> buffer)
+        {
+            if (this.bufferedBytes.IsEmpty)
+            {
+                this.bufferedBytes = ReadMoreBytes();
+            }
+
+            int bytesToCopy = Math.Min(this.bufferedBytes.Length, buffer.Length);
+
+            this.bufferedBytes.Span.Slice(0, bytesToCopy).CopyTo(buffer);
+            this.bufferedBytes = this.bufferedBytes.Slice(bytesToCopy);
+
+            return bytesToCopy;
+        }
+#endif
+
+        private byte[] ReadMoreBytes()
+        {
+            byte[] array = new byte[3];
+            int bytesOffset = 0;
+
+            do
+            {
+                int bytesRead = base.Read(array, bytesOffset, 3 - bytesOffset);
+
+                bytesOffset += bytesRead;
+
+                if (bytesRead == 0)
+                {
+                    return array.AsSpan(0, bytesOffset).ToArray();
+                }
+            }
+            while (bytesOffset < 3);
+
+            return array;
+        }
     }
 }
