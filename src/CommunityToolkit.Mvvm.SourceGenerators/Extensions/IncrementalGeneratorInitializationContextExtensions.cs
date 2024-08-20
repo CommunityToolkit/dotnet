59190d46a2cc39af0af736ae1e40694a4e403654
Sergio Pedri <sergio0694@live.com>
2024-08-20 11:58:11 -0700

    Add new FAWMN variant with global options

--- src/CommunityToolkit.Mvvm.SourceGenerators/Extensions/IncrementalGeneratorInitializationContextExtensions.cs
+++ src/CommunityToolkit.Mvvm.SourceGenerators/Extensions/IncrementalGeneratorInitializationContextExtensions.cs
@@ -4,7 +4,11 @@
 
 using System;
 using System.Collections.Immutable;
+using System.Threading;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+#pragma warning disable CS1574
 
 namespace CommunityToolkit.Mvvm.SourceGenerators.Extensions;
 
@@ -13,6 +17,31 @@ namespace CommunityToolkit.Mvvm.SourceGenerators.Extensions;
 /// </summary>
 internal static class IncrementalGeneratorInitializationContextExtensions
 {
+    /// <inheritdoc cref="SyntaxValueProvider.ForAttributeWithMetadataName"/>
+    public static IncrementalValuesProvider<T> ForAttributeWithMetadataNameAndOptions<T>(
+        this IncrementalGeneratorInitializationContext context,
+        string fullyQualifiedMetadataName,
+        Func<SyntaxNode, CancellationToken, bool> predicate,
+        Func<GeneratorAttributeSyntaxContextWithOptions, CancellationToken, T> transform)
+    {
+        // Invoke 'ForAttributeWithMetadataName' normally, but just return the context directly
+        IncrementalValuesProvider<GeneratorAttributeSyntaxContext> syntaxContext = context.SyntaxProvider.ForAttributeWithMetadataName(
+            fullyQualifiedMetadataName,
+            predicate,
+            static (context, token) => context);
+
+        // Do the same for the analyzer config options
+        IncrementalValueProvider<AnalyzerConfigOptions> configOptions = context.AnalyzerConfigOptionsProvider.Select(static (provider, token) => provider.GlobalOptions);
+
+        // Merge the two and invoke the provided transform on these two values. Neither value
+        // is equatable, meaning the pipeline will always re-run until this point. This is
+        // intentional: we don't want any symbols or other expensive objects to be kept alive
+        // across incremental steps, especially if they could cause entire compilations to be
+        // rooted, which would significantly increase memory use and introduce more GC pauses.
+        // In this specific case, flowing non equatable values in a pipeline is therefore fine.
+        return syntaxContext.Combine(configOptions).Select((input, token) => transform(new GeneratorAttributeSyntaxContextWithOptions(input.Left, input.Right), token));
+    }
+
     /// <summary>
     /// Conditionally invokes <see cref="IncrementalGeneratorInitializationContext.RegisterSourceOutput{TSource}(IncrementalValueProvider{TSource}, Action{SourceProductionContext, TSource})"/>
     /// if the value produced by the input <see cref="IncrementalValueProvider{TValue}"/> is <see langword="true"/>.
