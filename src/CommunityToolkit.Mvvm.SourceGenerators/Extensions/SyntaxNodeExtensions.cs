c8e794beeefdf89c95770727f149da6a588d4f5d
Sergio Pedri <sergio0694@live.com>
2024-10-22 19:57:43 -0700

    Add logic to match partial properties

--- src/CommunityToolkit.Mvvm.SourceGenerators/Extensions/SyntaxNodeExtensions.cs
+++ src/CommunityToolkit.Mvvm.SourceGenerators/Extensions/SyntaxNodeExtensions.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace CommunityToolkit.Mvvm.SourceGenerators.Extensions;
 
@@ -29,5 +31,31 @@ internal static class SyntaxNodeExtensions
             symbol.DeclaringSyntaxReferences is [SyntaxReference syntaxReference, ..] &&
             syntaxReference.SyntaxTree == syntaxNode.SyntaxTree &&
             syntaxReference.Span == syntaxNode.Span;
+    }
+
+    /// <summary>
+    /// Checks whether a given <see cref="SyntaxNode"/> is a given type declaration with or potentially with any base types, using only syntax.
+    /// </summary>
+    /// <typeparam name="T">The type of declaration to check for.</typeparam>
+    /// <param name="node">The input <see cref="SyntaxNode"/> to check.</param>
+    /// <returns>Whether <paramref name="node"/> is a given type declaration with or potentially with any base types.</returns>
+    public static bool IsTypeDeclarationWithOrPotentiallyWithBaseTypes<T>(this SyntaxNode node)
+        where T : TypeDeclarationSyntax
+    {
+        // Immediately bail if the node is not a type declaration of the specified type
+        if (node is not T typeDeclaration)
+        {
+            return false;
+        }
+
+        // If the base types list is not empty, the type can definitely has implemented interfaces
+        if (typeDeclaration.BaseList is { Types.Count: > 0 })
+        {
+            return true;
+        }
+
+        // If the base types list is empty, check if the type is partial. If it is, it means
+        // that there could be another partial declaration with a non-empty base types list.
+        return typeDeclaration.Modifiers.Any(SyntaxKind.PartialKeyword);
     }
 }
