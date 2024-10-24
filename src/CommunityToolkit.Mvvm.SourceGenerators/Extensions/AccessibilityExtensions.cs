b8e1435f360b96c5987e189cc7b0593d5affea9d
Sergio Pedri <sergio0694@live.com>
2024-10-23 21:11:29 -0700

    Generalizing the generated accessibility modifiers

---
+++ src/CommunityToolkit.Mvvm.SourceGenerators/Extensions/AccessibilityExtensions.cs
@@ -0,0 +1,35 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace CommunityToolkit.Mvvm.SourceGenerators.Extensions;
+
+/// <summary>
+/// Extension methods for the <see cref="Accessibility"/> type.
+/// </summary>
+internal static class AccessibilityExtensions
+{
+    /// <summary>
+    /// Converts a given <see cref="Accessibility"/> value to the equivalent <see cref="SyntaxTokenList"/>."/>
+    /// </summary>
+    /// <param name="accessibility">The input <see cref="Accessibility"/> value to convert.</param>
+    /// <returns>The <see cref="SyntaxTokenList"/> representing the modifiers for <paramref name="accessibility"/>.</returns>
+    public static SyntaxTokenList ToSyntaxTokenList(this Accessibility accessibility)
+    {
+        return accessibility switch
+        {
+            Accessibility.NotApplicable => TokenList(),
+            Accessibility.Private => TokenList(Token(SyntaxKind.PrivateKeyword)),
+            Accessibility.ProtectedAndInternal => TokenList(Token(SyntaxKind.PrivateKeyword), Token(SyntaxKind.InternalKeyword)),
+            Accessibility.Protected => TokenList(Token(SyntaxKind.ProtectedKeyword)),
+            Accessibility.Internal => TokenList(Token(SyntaxKind.InternalKeyword)),
+            Accessibility.ProtectedOrInternal => TokenList(Token(SyntaxKind.ProtectedKeyword), Token(SyntaxKind.InternalKeyword)),
+            Accessibility.Public => TokenList(Token(SyntaxKind.PublicKeyword)),
+            _ => TokenList()
+        };
+    }
+}
